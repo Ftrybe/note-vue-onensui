8589339f1273b8040d13e4ceaf3b4a784ba23d2a
Ftrybe <fcbyeah@yeah.net>
2019-09-23 22:56:51 +0800

    login

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,6 +1,19 @@
 <template>
     <v-ons-page>
-       
+       <div>
+           这是盒子
+           <div>记事(动效)</div>
+           <div>
+               表单
+               <div>用户名</div>
+               <div>密码</div>
+           </div>
+           <div>登陆按钮</div>
+       </div>
+       <div>
+           <div>qq快捷登陆</div>
+           <div>weixin快捷登陆</div>
+       </div>
 	</v-ons-page>
 </template>
 <script lang='ts'>
