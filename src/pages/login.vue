ade5cbde6847f8de3b02beec7b2068a7eafe2435
ftrybe <fcbyeah@yeah.net>
2019-09-24 17:30:24 +0800

    架子

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,29 +1,40 @@
 <template>
-    <v-ons-page>
-       <div>
-           这是盒子
-           <div>记事(动效)</div>
-           <div>
-               表单
-               <div>用户名</div>
-               <div>密码</div>
-           </div>
-           <div>登陆按钮</div>
-       </div>
-       <div>
-           <div>qq快捷登陆</div>
-           <div>weixin快捷登陆</div>
-       </div>
-	</v-ons-page>
+  <v-ons-page>
+    <div class="login">
+      <div class>
+        <object data="svg/login.svg" type="image/svg+xml" width="200px" height="100px"></object>
+      </div>
+      <div>
+        <div class="login__standard">
+          <div>
+            <v-ons-input placeholder="账号" float />
+          </div>
+          <div>
+            <v-ons-input placeholder="密码" float />
+          </div>
+        </div>
+         <div class="login__phone">
+          <div>
+            <v-ons-input placeholder="手机号" float />
+          </div>
+          <div>
+            <v-ons-input placeholder="密码" float />
+          </div>
+        </div>
+      </div>
+    <v-ons-button>登录</v-ons-button>
+    </div>
+    <div>
+        <v-ons-icon icon="fa-qq"></v-ons-icon>
+        <v-ons-icon icon="fa-weixin"></v-ons-icon>
+    </div>
+  </v-ons-page>
 </template>
 <script lang='ts'>
-import { Component, Vue } from 'vue-property-decorator'
+import { Component, Vue } from "vue-property-decorator";
 
 @Component
-export default class LoginPage extends Vue {
-
-}
+export default class LoginPage extends Vue {}
 </script>
 <style scoped lang='scss'>
-
 </style>
